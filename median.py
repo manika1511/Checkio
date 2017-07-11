90f5dd6808c7c1a0002caa7e1afc7ba8939b8383
manika15 <manika.kapoor90@gmail.com>
2017-07-10 17:14:29 -0700

    add median

---
+++ median.py
@@ -0,0 +1,18 @@
+def checkio(data):
+    data.sort()
+    length = len(data)
+    if length %2 != 0:
+        return data[int((length-1)/2)]
+    else:
+        median = (data[int((length-1)/2)] + data[int((length)/2)])/2
+        return median
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([1, 2, 3, 4, 5]) == 3, "Sorted list"
+    assert checkio([3, 1, 2, 5, 3]) == 3, "Not sorted list"
+    assert checkio([1, 300, 2, 200, 1]) == 2, "It's not an average"
+    assert checkio([3, 6, 20, 99, 10, 15]) == 12.5, "Even length"
+    print("Start the long test")
+    assert checkio(list(range(1000000))) == 499999.5, "Long."
+    print("The local tests are done.")
