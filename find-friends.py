523c17ec5f9777ff06a6e2b7f7dedbc8a1cc8e2d
manika15 <manika.kapoor90@gmail.com>
2017-07-12 17:48:02 -0700

    add how to find friends

---
+++ find-friends.py
@@ -0,0 +1,24 @@
+def check_connection(network, first, second):
+    element = {first}
+    for e in network:
+        for item in network:
+            l = item.split('-')
+            if l[0] in element or l[1] in element:
+                element.add(l[0])
+                element.add(l[1])
+    return second in element
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "scout2", "scout3") == True, "Scout Brotherhood"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "super", "scout2") == True, "Super Scout"
+    assert check_connection(
+        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
+         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
+        "dr101", "sscout") == False, "I don't know any scouts."
